3d498069ee1ad1c9fb3e857b5ec31c5913a3ce2b
Your Name <you@example.com>
2022-08-04 00:57:36 +0700

    a

---
+++ Enemies/script.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("canvas1");
+const ctx  = canvas.getContext('2d')
+const CANVAS_HEIGHT = canvas.height = 880;
+const CANVAS_WIDTH = canvas.width = 500;
+class Enemy {
+    constructor(){
+        this.x = Math.random() * canvas.width;       
+        this.y = Math.random() * canvas.height;
+        this.width = 100;
+        this.height = 100;
+
+    }
+    update()
+    {
+        this.x ++;
+        this.y ++;
+    }
+    draw(){
+        ctx.fillRect(enemy1.x,enemy1.y,enemy1.width,enemy1.height)
+        
+    }
+}
+    const enemy1 = new Enemy();
+
+function animate () {
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT)
+    enemy1.update();
+    enemy1.draw();
+    requestAnimationFrame(animate)
+    }
+animate()
