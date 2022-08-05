10c0d65b01bba6b457c8f5dd9d37cd0e9edbd862
Your Name <you@example.com>
2022-08-05 18:42:38 +0700

    a

--- Main-game/player.js
+++ Main-game/player.js
@@ -1,3 +1,4 @@
+import { Sitting ,Running,Jumping,Falling} from "./playerState.js";
 export class Player{
     constructor(game){
         this.game = game ;
@@ -8,23 +9,43 @@ export class Player{
         this.vy = 0;
         this.image = player;
         this.speed = 0;
+    
         this.maxSpeed = 10
+        this.weight = 1;
+        this.states = [new Sitting(this),new Running(this), new Jumping(this) , new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
+        this.frameX = 0;
+        this.frameY = 0;
 
     }   
     update(input)
-    {       
+    
+    {   this.currentState.handlerInput(input)
+               // horizontal movemont
         this.x += this.speed
         if(input.includes('ArrowRight')) this.speed = this.maxSpeed;
             else if(input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
             else this.speed = 0;
         if( this.x < 0 ) this.x= 0;
         if(this.x > this.game.width - this.width ) this.x =this.game.width - this.width;         
-
+        // vertical movemont
+        if(input.includes("ArrowUp") && this.onGround()) this.vy -= 30;
+        this.y += this.vy
+        if(!this.onGround() ) this.vy += this.weight;
+        else this.vy = 0;
     }
     draw(context){
-        context.drawImage(this.image,0,0,this.width,this.height   ,this.x,this.y,this.width,this.height )
+        context.drawImage(this.image,this.frameX* this.width,this.frameY*this.height,this.width,this.height   ,this.x,this.y,this.width,this.height )
         
        
        
+    }
+    onGround(){
+        return this.y >= this.game.height -this.height;
+    }
+    setState(state){
+        this.currentState = this.state[state];
+        this.enter();
     }
 }
---
+++ Main-game/playerState.js
@@ -0,0 +1,83 @@
+    const states = {
+        SITTING : 0,
+        RUNNING : 1 ,
+        JUMPING : 2,
+        FALLING : 3
+    }
+   
+
+    class State { 
+            contructor (state){
+                this.state = state;
+            }
+        }
+      
+     export class Sitting extends State {
+        constructor(player) {
+          super('SITTING');
+          this.player = player
+        }
+        enter(){
+            this.player.frameY = 5;
+        }
+        handlerInput(input){
+            if(input.includes("ArrowLeft") || input.includes("ArrrowRight")){
+                this.player.setState(states.RUNNING);
+            }
+
+        }
+       
+      }
+     export class Running extends State {
+        constructor(player) {
+          super('RUNNING');
+          this.player = player
+        }
+        enter(){
+            this.player.frameY = 3;
+        }
+        handlerInput(input){
+            if(input.includes("ArrowDown") ){
+                this.player.setState(states.SITTING);
+            }
+            else if(input.includes("ArrowUP")){
+                this.player.setState(states.JUMPING);
+            }
+
+        }
+       
+      }
+     export class Jumping extends State {
+        constructor(player) {
+          super('JUMPING');
+          this.player = player
+        }
+        enter(){
+            this.player.frameY = 1;
+            if(this.player.onGround()) this.player.vy -= 30;
+        }
+        handlerInput(input){
+            if(input.player.vy > this.player.weight ){
+                this.player.setState(states.FALLLING);
+            }
+
+        }
+       
+      }
+     export class Falling extends State {
+        constructor(player) {
+          super('FALLING');
+          this.player = player
+        }
+        enter(){
+            this.player.frameY = 2;
+            if(this.player.onGround()) this.player.vy -= 30;
+        }
+        handlerInput(input){
+            if(this.player.onGround() ){
+                this.player.setState(states.RUNNING);
+            }
+
+        }
+       
+      }
